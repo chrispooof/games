1593f2f509f1f797bc9dab4a756ccacb8b8659b9
Christian Bjerre-Fernandes <chrisfernandes18@gmail.com>
2026-03-19 14:22:39 -0400

    fix: GitHub Pages rendering

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  base: "/games/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 })
