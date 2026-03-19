740ed6f7624443bab198537d8ab4fcc662992e3c
Christian Bjerre-Fernandes <chrisfernandes18@gmail.com>
2026-03-19 13:26:33 -0400

    feat: Nertz tile cover image and added coming soon tile

--- ui/app/components/game-tile.tsx
+++ ui/app/components/game-tile.tsx
@@ -1,15 +1,20 @@
 import { NavLink } from "react-router"
+import ComingSoon from "~/assets/coming-soon.png"
 
 interface GameTileProps {
   name: string
   url?: string
+  img?: string
 }
 
-const GameTile = ({ name, url }: GameTileProps) => {
+const GameTile = ({ name, url, img }: GameTileProps) => {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
-      <NavLink to={`/nertz`}>
-        {url && <img src={url} alt={name} className="w-full h-48 object-cover rounded-lg" />}
+      <NavLink to={`${url}`}>
+        {img && <img src={img} alt={name} className="w-full h-48 object-contain rounded-lg" />}
+        {!img && (
+          <img src={ComingSoon} alt={name} className="w-full h-48 object-contain rounded-lg" />
+        )}
         <h2 className="text-xl font-bold mt-2">{name}</h2>
       </NavLink>
     </div>
--- ui/app/screens/home.tsx
+++ ui/app/screens/home.tsx
@@ -1,11 +1,12 @@
 import GameTile from "~/components/game-tile"
+import nertzImg from "~/assets/nertz.png"
 
 const HomeScreen = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Home</h1>
       <div>
-        <GameTile name="Nertz" url="/nertz" />
+        <GameTile name="Nertz" url="/nertz" img={nertzImg} />
       </div>
     </div>
   )
